6fb636a22cddfb9de20973c42bf6ab547d8f36db
Matthias Klass <matthias@klass.li>
2012-01-24 17:47:50 +0100

    SSL support

--- src/li/klass/fhem/fhem/FHEMWebConnection.java
+++ src/li/klass/fhem/fhem/FHEMWebConnection.java
@@ -33,22 +33,34 @@ import li.klass.fhem.exception.HostConnectionException;
 import li.klass.fhem.exception.TimeoutException;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.ClientConnectionManager;
 import org.apache.http.conn.ConnectTimeoutException;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
+import org.apache.http.protocol.HTTP;
 
 import java.net.URI;
 import java.net.URLEncoder;
+import java.security.KeyStore;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class FHEMWebConnection implements FHEMConnection {
 
+    public static final int CONNECTION_TIMEOUT = 3000;
+    public static final int SOCKET_TIMEOUT = 20000;
     private DefaultHttpClient client;
 
     public static final String FHEMWEB_URL = "FHEMWEB_URL";
@@ -61,7 +73,7 @@ public class FHEMWebConnection implements FHEMConnection {
         HttpParams httpParams = new BasicHttpParams();
         HttpConnectionParams.setConnectionTimeout(httpParams, 3000);
         HttpConnectionParams.setSoTimeout(httpParams, 20000);
-        client = new DefaultHttpClient(httpParams);
+        client = createNewHTTPClient();
     }
 
     @Override
@@ -150,5 +162,31 @@ public class FHEMWebConnection implements FHEMConnection {
     private String getPassword() {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(AndFHEMApplication.getContext());
         return sharedPreferences.getString(FHEMWEB_PASSWORD, "");
+    }
+
+    private DefaultHttpClient createNewHTTPClient() {
+        try {
+            KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            trustStore.load(null, null);
+
+            SSLSocketFactory sf = new CustomSSLSocketFactory(trustStore);
+            sf.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+
+            HttpParams params = new BasicHttpParams();
+            HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
+            HttpProtocolParams.setContentCharset(params, HTTP.UTF_8);
+            HttpConnectionParams.setConnectionTimeout(params, CONNECTION_TIMEOUT);
+            HttpConnectionParams.setSoTimeout(params, SOCKET_TIMEOUT);
+
+            SchemeRegistry registry = new SchemeRegistry();
+            registry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
+            registry.register(new Scheme("https", sf, 443));
+
+            ClientConnectionManager ccm = new ThreadSafeClientConnManager(params, registry);
+
+            return new DefaultHttpClient(ccm, params);
+        } catch (Exception e) {
+            return new DefaultHttpClient();
+        }
     }
 }
