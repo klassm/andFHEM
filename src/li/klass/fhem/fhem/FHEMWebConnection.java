3a8390522fb07117653c82cafc0a26a90351cec6
Matthias Klass <matthias@klass.li>
2012-03-01 10:15:42 +0100

    error message if the XHR tag in FHEMWEB is unsupported

--- src/li/klass/fhem/fhem/FHEMWebConnection.java
+++ src/li/klass/fhem/fhem/FHEMWebConnection.java
@@ -28,9 +28,7 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import li.klass.fhem.AndFHEMApplication;
-import li.klass.fhem.exception.AuthenticationException;
-import li.klass.fhem.exception.HostConnectionException;
-import li.klass.fhem.exception.TimeoutException;
+import li.klass.fhem.exception.*;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpVersion;
@@ -117,10 +115,14 @@ public class FHEMWebConnection implements FHEMConnection {
                 throw new AuthenticationException(response.getStatusLine().toString());
             }
             content = IOUtils.toString(response.getEntity().getContent());
+
+            if (content.contains("<title>")) {
+                throw new FHEMUpdateRequiredException();
+            }
+        } catch (AndFHEMException e) {
+            throw e;
         } catch (ConnectTimeoutException e) {
             throw new TimeoutException(e);
-        } catch (AuthenticationException e) {
-            throw e;
         } catch (Exception e) {
             throw new HostConnectionException(e);
         }
