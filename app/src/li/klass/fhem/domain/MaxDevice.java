ac3ae035566cc8fc2d17f09fa902d85fed9133c7
Matthias Klass <matthias@klass.li>
2013-01-12 17:05:25 +0100

    #282 Support WallMountedThermostat MAX devices

--- app/src/li/klass/fhem/domain/MaxDevice.java
+++ app/src/li/klass/fhem/domain/MaxDevice.java
@@ -102,7 +102,7 @@ public class MaxDevice extends ToggleableDevice<MaxDevice> implements DesiredTem
             subType = SubType.CUBE;
         } else if (value.equalsIgnoreCase("PushButton")) {
             subType = SubType.SWITCH;
-        } else if (value.equalsIgnoreCase("HeatingThermostat")) {
+        } else if (value.equalsIgnoreCase("HeatingThermostat") || value.equalsIgnoreCase("WallMountedThermostat")) {
             subType = SubType.TEMPERATURE;
         }
 
