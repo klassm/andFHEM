fe1db4ec37e14c8110dd5dc6d518a49227564dea
Matthias Klass <matthias@klass.li>
2013-02-19 16:06:44 +0100

    #336 THSensor Homematic changed log structure

--- app/src/li/klass/fhem/domain/CULHMDevice.java
+++ app/src/li/klass/fhem/domain/CULHMDevice.java
@@ -386,8 +386,8 @@ public class CULHMDevice extends DimmableContinuousStatesDevice<CULHMDevice> imp
             case TEMPERATURE_HUMIDITY:
 
                 addDeviceChartIfNotNull(new DeviceChart(R.string.temperatureHumidityGraph,
-                        ChartSeriesDescription.getRegressionValuesInstance(R.string.temperature, "4:", R.string.yAxisTemperature),
-                        new ChartSeriesDescription(R.string.humidity, "6:", R.string.yAxisHumidity)), humidity, measuredTemp);
+                        ChartSeriesDescription.getRegressionValuesInstance(R.string.temperature, "4:measured-temp:0", R.string.yAxisTemperature),
+                        new ChartSeriesDescription(R.string.humidity, "4:humidity:0", R.string.yAxisHumidity)), humidity, measuredTemp);
 
                 break;
 
