8da08a36f9efaa7b3966f262c773405c95f0f65c
Matthias Klass <matthias@klass.li>
2013-06-08 11:17:54 +0200

    #427 add GPIO4 device support (graph)

--- app/src/li/klass/fhem/domain/GPIO4Device.java
+++ app/src/li/klass/fhem/domain/GPIO4Device.java
@@ -1,9 +1,14 @@
 package li.klass.fhem.domain;
 
+import li.klass.fhem.R;
 import li.klass.fhem.appwidget.annotation.ResourceIdMapper;
 import li.klass.fhem.domain.core.Device;
+import li.klass.fhem.domain.core.DeviceChart;
 import li.klass.fhem.domain.genericview.ShowField;
+import li.klass.fhem.service.graph.description.ChartSeriesDescription;
 import li.klass.fhem.util.ValueDescriptionUtil;
+
+import java.util.List;
 
 @SuppressWarnings("unused")
 public class GPIO4Device extends Device<GPIO4Device> {
@@ -29,6 +34,16 @@ public class GPIO4Device extends Device<GPIO4Device> {
 
     public String getTemperature() {
         return temperature;
+    }
+
+    @Override
+    protected void fillDeviceCharts(List<DeviceChart> chartSeries) {
+        super.fillDeviceCharts(chartSeries);
+
+        if (subType == SubType.TEMPERATURE) {
+            addDeviceChartIfNotNull(new DeviceChart(R.string.temperatureGraph,
+                    ChartSeriesDescription.getRegressionValuesInstance(R.string.temperature, "4:T", R.string.yAxisTemperature)), temperature);
+        }
     }
 
     @Override
