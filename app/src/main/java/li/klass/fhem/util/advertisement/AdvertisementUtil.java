176e702d6693ab974e191e2125dc42e902188959
Matthias Klass <matthias@klass.li>
2013-11-03 17:44:26 +0100

    #516 Correct spelling of selfad image filename

--- app/src/main/java/li/klass/fhem/util/advertisement/AdvertisementUtil.java
+++ app/src/main/java/li/klass/fhem/util/advertisement/AdvertisementUtil.java
@@ -2,13 +2,13 @@
  * AndFHEM - Open Source Android application to control a FHEM home automation
  * server.
  *
- * Copyright (c) 2012, Matthias Klass or third-party contributors as
+ * Copyright (c) 2011, Matthias Klass or third-party contributors as
  * indicated by the @author tags or express copyright attribution
  * statements applied by the authors.  All third-party contributions are
  * distributed under license by Red Hat Inc.
  *
  * This copyrighted material is made available to anyone wishing to use, modify,
- * copy, or redistribute it subject to the terms and conditions of the GNU GENERAL PUBLICLICENSE, as published by the Free Software Foundation.
+ * copy, or redistribute it subject to the terms and conditions of the GNU GENERAL PUBLIC LICENSE, as published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
@@ -19,6 +19,7 @@
  * along with this distribution; if not, write to:
  *   Free Software Foundation, Inc.
  *   51 Franklin Street, Fifth Floor
+ *   Boston, MA  02110-1301  USA
  */
 
 package li.klass.fhem.util.advertisement;
@@ -26,11 +27,16 @@ package li.klass.fhem.util.advertisement;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Configuration;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import com.google.ads.*;
+
+import com.google.ads.Ad;
+import com.google.ads.AdListener;
+import com.google.ads.AdRequest;
+import com.google.ads.AdSize;
+import com.google.ads.AdView;
+
 import li.klass.fhem.AndFHEMApplication;
 import li.klass.fhem.R;
 import li.klass.fhem.constants.Actions;
@@ -88,7 +94,7 @@ public class AdvertisementUtil {
     }
 
     private static void addErrorView(final Activity activity, LinearLayout container) {
-        ImageView selfAd = (ImageView) activity.getLayoutInflater().inflate(R.layout.selfadd, null);
+        ImageView selfAd = (ImageView) activity.getLayoutInflater().inflate(R.layout.selfad, null);
         selfAd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
