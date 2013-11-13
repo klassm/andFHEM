f9a3c30bbb9f70395da34397e2390c533c62caf5
Matthias Klass <matthias@klass.li>
2013-11-13 13:50:41 +0100

    #535 Eltako Switching / Shutter

--- app/src/main/java/li/klass/fhem/adapter/devices/core/GenericDeviceAdapter.java
+++ app/src/main/java/li/klass/fhem/adapter/devices/core/GenericDeviceAdapter.java
@@ -43,6 +43,7 @@ import android.widget.TextView;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -164,7 +165,8 @@ public class GenericDeviceAdapter<D extends Device<D>> extends DeviceAdapter<D> 
                 notifyFieldListeners(context, device, layout, "webcmd", webCmdTableRow);
             }
 
-            List<Field> declaredFields = DeviceFields.sortedFieldsOf(device.getClass());
+            List<Field> declaredFields = Arrays.asList(device.getClass().getDeclaredFields());
+            DeviceFields.sort(declaredFields);
 
             for (Field declaredField : declaredFields) {
                 declaredField.setAccessible(true);
