fc4ba9e6822993a88a691e514bc1028a34ebb5bc
Matthias Klass <matthias@klass.li>
2013-12-08 15:59:32 +0100

    better navbar handling

--- js/layout.js
+++ js/layout.js
@@ -1,24 +1,25 @@
-$(document).ready(function() {
+$(document).ready(function() {    
+    navbarWidth = ($("#navbar").css("width").replace("px", "") * 1);
+    
+    animationTime = 500;
+    
     isNavbarPermanent = false;
-    navbarVisibleHeight = "100%";
-    navbarInvisibleHeight = "0px";
-         
-    hideNavbar = function() {
-      if (isNavbarPermanent) return;
-      
-      if (isClickLinkTarget(event.target)) return;
-      
-      var navbar = document.getElementById("navbar");
-      var height = navbar.style.height;
-      if (height != "0px") {
-        navbar.style.height = "0px";
-      }
-    }
     
     toggleNavbarVisibility = function() {
       if (isNavbarPermanent) return;
       
       var navbar = $("#navbar");
+      
+      var visible = isNavbarVisible();
+      
+      if (visible) {
+        hideNavbar();
+      } else {
+        showNavbar(true);
+      }
+      
+      return;
+      
       var height = navbar.css("height");
       var targetHeight;
       if (height != navbarInvisibleHeight && !! height) {
@@ -29,34 +30,75 @@ $(document).ready(function() {
       navbar.css("height", targetHeight);
     }
     
+    hideNavbar = function() {
+      if (isNavbarPermanent) return;
+      
+      if (isClickLinkTarget(event.target)) return;
+      
+      var navbar = $("#navbar");
+      if (!! isNavbarVisible()) {
+        var targetLeft = (navbarWidth * -1) + "px";
+        navbar.animate({
+          left: targetLeft
+        }, animationTime);
+      }
+    }
+    
+    showNavbar = function(animate) {
+      var time = animationTime;
+      if (! animate) time = 0;
+      
+      var navbar = $("#navbar");
+      navbar.css("height", $(document).height());
+      if (! isNavbarVisible()) {
+        navbar.animate({
+          left: "0px"
+        }, time);
+      }
+    }
+    
+    isNavbarVisible = function() {
+      var left = $("#navbar").css("left");
+      return ! (left && left != "0px");
+    }
+    
     function isClickLinkTarget(target) {
       if (! target) return false;
       if (target.nodeName == "A") return true;
       return isClickLinkTarget(target.parentElement);
     }
     
-    function handlePermanentNavbar() {
+    function handlePermanentNavbar(animate) {
       var width = $(window).width();
       var height = $(window).height();
       
       var navbar = $("#navbar");
       var content = $("#body");
       
+      navbar.css("height", $(document).height()); 
+      
       isNavbarPermanent = (width > height && width > 800);
       
+      var time = animationTime;
+      if (! animate) time = 0;
+      
       if (isNavbarPermanent) {
-        navbar.css("height", $(document).height());              
-        content.css("margin-left", "240px");
+        showNavbar(animate);
+        content.animate({
+          "margin-left": navbarWidth + "px"
+        }, time);
       } else {
-        navbar.css("height", navbarInvisibleHeight); 
-        content.css("margin-left", "0px");
+        hideNavbar();
+        
+        content.animate({
+          "margin-left": "0px"
+        }, time);
       }
     }
     
     $(window).resize(function() {
-      handlePermanentNavbar();
+      handlePermanentNavbar(true);
     });
     
-    handlePermanentNavbar();
-    
+    handlePermanentNavbar(false);
 });
