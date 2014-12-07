9bb1b3b063409d5ed6eccbdf33af65e1ac06f865
Matthias Klass <matthias@klass.li>
2014-12-07 16:23:19 +0100

    #816 send application log as attachment (to enable bigger log sizes)

--- app/src/main/java/li/klass/fhem/error/ErrorHolder.java
+++ app/src/main/java/li/klass/fhem/error/ErrorHolder.java
@@ -26,12 +26,18 @@ package li.klass.fhem.error;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
 import android.util.Log;
 
+import com.google.common.base.Charsets;
 import com.google.common.base.Joiner;
-import com.google.common.base.Predicate;
 import com.google.common.io.CharStreams;
+import com.google.common.io.Files;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
 
@@ -40,26 +46,11 @@ import li.klass.fhem.R;
 import li.klass.fhem.util.CloseableUtil;
 import li.klass.fhem.util.DialogUtil;
 
-import static com.google.common.collect.Iterables.filter;
+import static com.google.common.collect.Lists.newArrayList;
 import static li.klass.fhem.util.StackTraceUtil.exceptionAsString;
 import static li.klass.fhem.util.StackTraceUtil.whereAmI;
 
 public class ErrorHolder {
-
-    public static final Predicate<String> LOG_INCLUDE_PREDICATE = new Predicate<String>() {
-        @Override
-        public boolean apply(String input) {
-            return !input.contains("D/li.klass") &&
-                    !input.contains("dalvikvm") &&
-                    !input.contains("W/Resources") &&
-                    !input.contains("requestLayout() improperly") &&
-                    !input.contains("V/li.klass");
-        }
-    };
-
-    private enum TailHandling {
-        FIRST, LAST
-    }
 
     private static final String TAG = ErrorHolder.class.getName();
 
@@ -103,56 +94,103 @@ public class ErrorHolder {
                 R.string.error_send_content, new DialogUtil.AlertOnClickListener() {
                     @Override
                     public void onClick() {
-                        String text = ErrorHolder.getText();
-                        if (text == null) {
-                            DialogUtil.showAlertDialog(context, R.string.error_send, R.string.error_send_no_error);
-                            return;
-                        }
-
-                        sendMail(context, "Send last error", "Error encountered!", text, TailHandling.FIRST);
+                        handleSendLastError(context);
                     }
                 });
     }
 
+    private static void handleSendLastError(Context context) {
+        if (!handleExternalStorageState(context)) return;
+        try {
+            String lastError = ErrorHolder.getText();
+            if (lastError == null) {
+                DialogUtil.showAlertDialog(context, R.string.error_send, R.string.error_send_no_error);
+                return;
+            }
+
+            File attachment = writeToDisk(context, lastError);
+            sendMail(context, "Send last error", "Error encountered!", deviceInformation(), Uri.fromFile(attachment));
+
+        } catch (Exception e) {
+            Log.e(TAG, "error while sending last error");
+        }
+    }
+
     @SuppressWarnings("unchecked")
     public static void sendApplicationLogAsMail(Context context) {
+        if (!handleExternalStorageState(context)) return;
+        try {
+            File file = writeApplicationLogToDisk(context);
+
+            sendMail(context, context.getString(R.string.application_log_send), "Send app log",
+                    deviceInformation(), Uri.fromFile(file));
+        } catch (Exception e) {
+            Log.e(TAG, "Error while reading application log", e);
+        }
+    }
+
+    private static void sendMail(Context context, String chooserText, String subject, String text,
+                                 Uri attachment) throws IOException {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{AndFHEMApplication.ANDFHEM_MAIL});
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+
+        intent.putExtra(android.content.Intent.EXTRA_STREAM, attachment);
+
+        if (context == null) return;
+
+        context.startActivity(Intent.createChooser(intent, chooserText));
+    }
+
+    private static File writeApplicationLogToDisk(Context context) throws IOException {
         InputStreamReader reader = null;
         try {
+            File outputDir = context.getCacheDir();
+            File outputFile = File.createTempFile("andFHEM-", "log", outputDir);
+            outputFile.deleteOnExit();
+
             Process process = Runtime.getRuntime().exec("logcat -d");
             reader = new InputStreamReader(process.getInputStream());
 
             List<String> logLines = CharStreams.readLines(reader);
-            String log = Joiner.on("\r\n").join(filter(logLines, LOG_INCLUDE_PREDICATE));
-
-            sendMail(context, context.getString(R.string.application_log_send), "Send app log",
-                    log, TailHandling.LAST);
-        } catch (Exception e) {
-            Log.e(TAG, "Error while reading application log", e);
+            String log = Joiner.on("\r\n").join(logLines);
+            return writeToDisk(context, log);
         } finally {
             CloseableUtil.close(reader);
         }
     }
 
-    private static void sendMail(Context context, String chooserText, String subject, String text,
-                                 TailHandling tailHandling) {
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setType("text/plain");
-        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{AndFHEMApplication.ANDFHEM_MAIL});
-        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+    private static File writeToDisk(Context context, String content) throws IOException {
+        File outputDir = context.getExternalFilesDir(null);
+        File outputFile = File.createTempFile("andFHEM-", ".log", outputDir);
+        outputFile.setReadable(true, true);
+        outputFile.deleteOnExit();
 
-        // we have to limit the transaction size, see http://www.lonestarprod.com/?p=34
-        // for details.
-        if (text.length() > 20000) {
-            if (tailHandling == TailHandling.FIRST) {
-                text = text.substring(0, 20000);
-            } else {
-                text = text.substring(text.length() - 20000, text.length());
-            }
+        Files.write(content, outputFile, Charsets.UTF_8);
+
+        return outputFile;
+    }
+
+    private static String deviceInformation() {
+        return Joiner.on("\r\n").join(newArrayList(
+                "Device information:",
+                "OS-Version: " + System.getProperty("os.version"),
+                "API-Level: " + Build.VERSION.SDK_INT,
+                "Device: " + android.os.Build.DEVICE,
+                "Manufacturer: " + Build.MANUFACTURER,
+                "Model: " + android.os.Build.MODEL,
+                "Product: " + android.os.Build.PRODUCT
+        ));
+    }
+
+    private static boolean handleExternalStorageState(Context context) {
+        String state = Environment.getExternalStorageState();
+        if (!state.equals(Environment.MEDIA_MOUNTED)) {
+            DialogUtil.showAlertDialog(context, R.string.error, R.string.errorExternalStorageNotPresent);
+            return false;
         }
-        intent.putExtra(Intent.EXTRA_TEXT, text);
-
-        if (context == null) return;
-
-        context.startActivity(Intent.createChooser(intent, chooserText));
+        return true;
     }
 }
