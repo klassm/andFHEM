24eca9cbd2845f3ba5b241cb3ad2086a473f74f6
Matthias Klass <matthias@klass.li>
2014-12-21 12:20:19 +0100

    yml file for circle ci

--- ci/install-dependencies.sh
+++ ci/install-dependencies.sh
@@ -7,5 +7,12 @@ DEPS="$ANDROID_HOME/installed-dependencies"
 
 if [ ! -e $DEPS ]; then
   echo y | android update sdk -u -a -t android-21 &&
+  echo y | android update sdk -u -a -t platform-tools &&
+  echo y | android update sdk -u -a -t build-tools-21.1.1 &&
+  
+  echo y | android update sdk -u -a -t extra-android-support &&
+  echo y | android update sdk -u -a -t extra-google-google_play_services &&
+  echo y | android update sdk -u -a -t extra-google-m2repository &&
+  echo y | android update sdk -u -a -t extra-google-play_billing &&  
   touch $DEPS
 fi
