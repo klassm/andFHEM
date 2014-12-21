87333de133e5b9f368fa4c06aa6c79b24a6a4d34
Matthias Klass <matthias@klass.li>
2014-12-21 10:54:53 +0100

    yml file for circle ci

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -6,7 +6,6 @@ export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
 DEPS="$ANDROID_HOME/installed-dependencies"
 
 if [ ! -e $DEPS ]; then
-  cp -r /usr/local/android-sdk-linux $ANDROID_HOME &&
   echo y | android update sdk -u -a -t android-21 &&
   echo y | android update sdk -u -a -t platform-tools &&
   echo y | android update sdk -u -a -t build-tools-21.0.0 &&
