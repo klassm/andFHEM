071cb710e1a06bda72c04c6ad035172c53be458c
Matthias Klass <matthias.klass@gmail.com>
2015-03-21 13:30:55 +0100

    Basic AndroidTest infrastructure

---
+++ src/androidTest/java/li/klass/fhem/instrumentation/ConnectionEditAndroidTest.java
@@ -0,0 +1,57 @@
+/*
+ * AndFHEM - Open Source Android application to control a FHEM home automation
+ * server.
+ *
+ * Copyright (c) 2011, Matthias Klass or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU GENERAL PUBLIC LICENSE, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU GENERAL PUBLIC LICENSE
+ * for more details.
+ *
+ * You should have received a copy of the GNU GENERAL PUBLIC LICENSE
+ * along with this distribution; if not, write to:
+ *   Free Software Foundation, Inc.
+ *   51 Franklin Street, Fifth Floor
+ *   Boston, MA  02110-1301  USA
+ */
+
+package li.klass.fhem.instrumentation;
+
+import android.content.Intent;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+
+import li.klass.fhem.R;
+import li.klass.fhem.activities.AndFHEMMainActivity;
+import li.klass.fhem.constants.Actions;
+import li.klass.fhem.constants.BundleExtraKeys;
+import li.klass.fhem.fragments.FragmentType;
+
+public class ConnectionEditAndroidTest extends ActivityInstrumentationTestCase2<AndFHEMMainActivity> {
+    public ConnectionEditAndroidTest() {
+        super(AndFHEMMainActivity.class);
+    }
+
+    public void testBlablub() throws Exception {
+        AndFHEMMainActivity activity = getActivity();
+
+        activity.sendBroadcast(new Intent(Actions.SHOW_FRAGMENT)
+                .putExtra(BundleExtraKeys.FRAGMENT, FragmentType.CONNECTION_LIST));
+
+        getInstrumentation().waitForIdleSync();
+        Button button = (Button) activity.findViewById(R.id.create);
+
+//        while((button = (Button) activity.findViewById(R.id.create)) == null) {
+//            getInstrumentation().waitForIdleSync();
+//        }
+
+        assertNotNull(button);
+    }
+}
