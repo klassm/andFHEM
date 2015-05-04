7a10a51f6bf384a9071a56d388ea91a740f99e4a
Matthias Klass <matthias.klass@gmail.com>
2015-05-04 18:51:40 +0200

    try to fix circleci build

--- ci/install-dependencies.sh
+++ ci/install-dependencies.sh
@@ -13,6 +13,6 @@ export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
   echo y | android update sdk -u -a -t extra-android-support &&
   echo y | android update sdk -u -a -t extra-google-google_play_services &&
   echo y | android update sdk -u -a -t extra-google-m2repository &&
-  echo y | android update sdk -u -a -t extra-google-play_billing &&  
+  echo y | android update sdk -u -a -t extra-google-play_billing #&& 
 #  touch $DEPS
 #fi
