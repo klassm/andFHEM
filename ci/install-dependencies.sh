1981e5bbe235e94a639e6ad5c4f881fa0511b95d
Matthias Klass <matthias.klass@gmail.com>
2015-05-04 18:49:04 +0200

    try to fix circleci build

--- ci/install-dependencies.sh
+++ ci/install-dependencies.sh
@@ -3,9 +3,9 @@
 # Fix the CircleCI path
 export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
 
-DEPS="$ANDROID_HOME/installed-dependencies"
+#DEPS="$ANDROID_HOME/installed-dependencies"
 
-if [ ! -e $DEPS ]; then
+#if [ ! -e $DEPS ]; then
   echo y | android update sdk -u -a -t android-21 &&
   echo y | android update sdk -u -a -t platform-tools &&
   echo y | android update sdk -u -a -t build-tools-22.0.1 &&
@@ -15,4 +15,4 @@ if [ ! -e $DEPS ]; then
   echo y | android update sdk -u -a -t extra-google-m2repository &&
   echo y | android update sdk -u -a -t extra-google-play_billing &&  
   touch $DEPS
-fi
+#fi
