4a50d46145f665f7cab49914ad6001c0d3598a13
Matthias Klass <matthias.klass@gmail.com>
2015-05-06 10:20:54 +0200

    try to fix circleci build

--- ci/install-dependencies.sh
+++ ci/install-dependencies.sh
@@ -6,8 +6,11 @@ export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
 #DEPS="$ANDROID_HOME/installed-dependencies"
 
 #if [ ! -e $DEPS ]; then
-  echo y | android update sdk -u -a -t android-21 &&
   echo y | android update sdk -u -a -t platform-tools &&
+  echo y | android update sdk -u -a -t android-21 &&
+  echo y | android update sdk -u -a -t android-22 &&
+  echo y | android update sdk -u -a -t tool &&
+
   echo y | android update sdk -u -a -t build-tools-22.0.1 &&
   
   echo y | android update sdk -u -a -t extra-android-support &&
