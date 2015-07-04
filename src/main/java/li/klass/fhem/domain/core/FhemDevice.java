e5c9d87f4a0904db117c21fd6ec80efe4bc25f58
Matthias Klass <matthias.klass@gmail.com>
2015-07-04 13:18:58 +0200

    Group attribute used for device group captions overlaps with internal group attribute of devices #934 * Use explicitly attribute instead of any attribute named "group"

--- src/main/java/li/klass/fhem/domain/core/FhemDevice.java
+++ src/main/java/li/klass/fhem/domain/core/FhemDevice.java
@@ -71,7 +71,6 @@ public abstract class FhemDevice<T extends FhemDevice<T>> extends HookedDevice<T
     @ShowField(description = ResourceIdMapper.measured, showAfter = "definition")
     private String measured;
     private long lastMeasureTime = -1;
-    private String group;
     private Set<SvgGraphDefinition> svgGraphDefinitions = newHashSet();
 
     private boolean hasStatisticsDevice = false;
@@ -114,11 +113,6 @@ public abstract class FhemDevice<T extends FhemDevice<T>> extends HookedDevice<T
     @XmllistAttribute("EVENTMAP")
     public void setEventmap(String value) {
         parseEventMap(value);
-    }
-
-    @XmllistAttribute("GROUP")
-    public void setGroup(String value) {
-        group = value;
     }
 
     public void afterDeviceXMLRead(Context context) {
