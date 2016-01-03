177ef6433b4228e61f1b855d3427c4fa38f25c53
Matthias Klass <matthias.klass@gmail.com>
2016-01-03 14:18:29 +0100

    Install sdk 23

--- ci/install-dependencies.sh
+++ ci/install-dependencies.sh
@@ -11,6 +11,7 @@ echo "ANDROID_HOME=$ANDROID_HOME"
   echo y | android update sdk -u -a -t platform-tools &&
   echo y | android update sdk -u -a -t android-21 &&
   echo y | android update sdk -u -a -t android-22 &&
+  echo y | android update sdk -u -a -t android-23 &&
   echo y | android update sdk -u -a -t tool &&
 
   echo y | android update sdk -u -a -t build-tools-22.0.1 &&
