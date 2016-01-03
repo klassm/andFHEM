f2d8b7014abbddf19b6fba35f40a97184e44ad2a
Matthias Klass <matthias.klass@gmail.com>
2016-01-03 14:32:50 +0100

    Repair install script

--- ci/install-dependencies.sh
+++ ci/install-dependencies.sh
@@ -11,7 +11,6 @@ echo "ANDROID_HOME=$ANDROID_HOME"
   echo y | android update sdk -u -a -t platform-tools &&
   echo y | android update sdk -u -a -t android-21 &&
   echo y | android update sdk -u -a -t android-22 &&
-  echo y | android update sdk -u -a -t android-23 &&
   echo y | android update sdk -u -a -t tool &&
 
   echo y | android update sdk -u -a -t build-tools-22.0.1 &&
