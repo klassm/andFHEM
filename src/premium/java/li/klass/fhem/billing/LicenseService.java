41740f8a2abeb87127c7b46fdc3da4a76ac43692
Matthias Klass <matthias.klass@gmail.com>
2016-02-28 14:01:27 +0100

    remove advertisement classes from premium

---
+++ src/premium/java/li/klass/fhem/billing/LicenseService.java
@@ -0,0 +1,81 @@
+/*
+ * AndFHEM - Open Source Android application to control a FHEM home automation
+ * server.
+ *
+ * Copyright (c) 2011, Matthias Klass or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU GENERAL PUBLIC LICENSE, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU GENERAL PUBLIC LICENSE
+ * for more details.
+ *
+ * You should have received a copy of the GNU GENERAL PUBLIC LICENSE
+ * along with this distribution; if not, write to:
+ *   Free Software Foundation, Inc.
+ *   51 Franklin Street, Fifth Floor
+ *   Boston, MA  02110-1301  USA
+ */
+
+package li.klass.fhem.billing;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import javax.security.cert.X509Certificate;
+
+import li.klass.fhem.AndFHEMApplication;
+import li.klass.fhem.util.ApplicationProperties;
+
+import static li.klass.fhem.AndFHEMApplication.PREMIUM_PACKAGE;
+
+@Singleton
+public class LicenseService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LicenseService.class);
+
+    @Inject
+    public LicenseService() {
+    }
+
+    public void isPremium(final IsPremiumListener listener) {
+        listener.isPremium(true);
+    }
+
+    private boolean isPremiumInternal(boolean loadSuccessful) {
+        return true;
+    }
+
+    public static boolean isDebug(Context context) {
+        try {
+            PackageInfo pkgInfo = context.getPackageManager()
+                    .getPackageInfo(context.getPackageName(), PackageManager.GET_SIGNATURES);
+
+            for (Signature appSignature : pkgInfo.signatures) {
+                X509Certificate appCertificate = X509Certificate.getInstance(appSignature.toByteArray());
+                if (appCertificate.getSubjectDN().getName().contains("Android Debug")) {
+                    return true;
+                }
+            }
+        } catch (Exception e) {
+            LOGGER.error("isDebug() : some exception occurred while reading app signatures", e);
+        }
+        return false;
+    }
+
+    public interface IsPremiumListener {
+        void isPremium(boolean isPremium);
+    }
+}
---
+++ src/premium/java/li/klass/fhem/service/advertisement/AdvertisementService.java
@@ -0,0 +1,66 @@
+/*
+ * AndFHEM - Open Source Android application to control a FHEM home automation
+ * server.
+ *
+ * Copyright (c) 2011, Matthias Klass or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU GENERAL PUBLIC LICENSE, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU GENERAL PUBLIC LICENSE
+ * for more details.
+ *
+ * You should have received a copy of the GNU GENERAL PUBLIC LICENSE
+ * along with this distribution; if not, write to:
+ *   Free Software Foundation, Inc.
+ *   51 Franklin Street, Fifth Floor
+ *   Boston, MA  02110-1301  USA
+ */
+
+package li.klass.fhem.service.advertisement;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import li.klass.fhem.AndFHEMApplication;
+import li.klass.fhem.R;
+import li.klass.fhem.constants.Actions;
+import li.klass.fhem.constants.BundleExtraKeys;
+import li.klass.fhem.fragments.FragmentType;
+import li.klass.fhem.service.intent.LicenseIntentService;
+import li.klass.fhem.util.FhemResultReceiver;
+
+import static li.klass.fhem.constants.BundleExtraKeys.IS_PREMIUM;
+import static li.klass.fhem.constants.ResultCodes.SUCCESS;
+
+@Singleton
+public class AdvertisementService {
+    @Inject
+    public AdvertisementService() {
+    }
+
+    public void addAd(final View view, final Activity activity) {
+    }
+}
