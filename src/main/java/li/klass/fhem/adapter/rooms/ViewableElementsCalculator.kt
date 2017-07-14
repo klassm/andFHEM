72f62b51d9445280b181dcc2faf89b95883cc44d
Matthias Klass <matthias.klass@tngtech.com>
2017-07-14 08:18:17 +0200

    Devices in room "hidden" are shown in all devices #1130

--- src/main/java/li/klass/fhem/adapter/rooms/ViewableElementsCalculator.kt
+++ src/main/java/li/klass/fhem/adapter/rooms/ViewableElementsCalculator.kt
@@ -1,19 +1,23 @@
 package li.klass.fhem.adapter.rooms
 
 import android.content.Context
+import li.klass.fhem.constants.PreferenceKeys.SHOW_HIDDEN_DEVICES
 import li.klass.fhem.domain.core.DeviceFunctionality
 import li.klass.fhem.domain.core.FhemDevice
 import li.klass.fhem.domain.core.RoomDeviceList
+import li.klass.fhem.util.ApplicationProperties
 import li.klass.fhem.widget.deviceFunctionality.DeviceGroupHolder
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class ViewableElementsCalculator @Inject constructor(
-        private val deviceGroupHolder: DeviceGroupHolder
+        private val deviceGroupHolder: DeviceGroupHolder,
+        private val applicationProperties: ApplicationProperties
 ) {
     fun calculateElements(context: Context, roomDeviceList: RoomDeviceList): List<Element> {
 
+        val showHiddenDevices = applicationProperties.getBooleanSharedPreference(SHOW_HIDDEN_DEVICES, false, context)
         val visibleParents: List<String> = deviceGroupHolder.getVisible(context)
                 .map { it.getCaptionText(context) }
                 .toList()
@@ -25,7 +29,10 @@ class ViewableElementsCalculator @Inject constructor(
         val groupComparator = GroupComparator(DeviceFunctionality.UNKNOWN.getCaptionText(context), visibleParents)
         val elementsInGroup = (visibleParents + customParents)
                 .filter { it !in invisibleParents }
-                .map { parent -> Pair(parent, roomDeviceList.getDevicesOfFunctionality(parent)) }
+                .map { parent ->
+                    Pair(parent, roomDeviceList.getDevicesOfFunctionality(parent)
+                            .filter { !it.isInRoom("hidden") || showHiddenDevices })
+                }
                 .toMap()
 
         val groups = elementsInGroup.keys.sortedWith(groupComparator)
