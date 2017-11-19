afafcea394af202ef4c68a077c73eb5630ddeb41
Matthias Klass <matthias.klass@tngtech.com>
2017-11-19 10:24:24 +0100

    Allow two lines in status appwidget #1196

---
+++ src/main/java/li/klass/fhem/appwidget/ui/widget/medium/StatusWidgetView.kt
@@ -0,0 +1,51 @@
+/*
+ * AndFHEM - Open Source Android application to control a FHEM home automation
+ * server.
+ *
+ * Copyright (c) 2011, Matthias Klass or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU GENERAL PUBLIC LICENSE, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU GENERAL PUBLIC LICENSE
+ * for more details.
+ *
+ * You should have received a copy of the GNU GENERAL PUBLIC LICENSE
+ * along with this distribution; if not, write to:
+ *   Free Software Foundation, Inc.
+ *   51 Franklin Street, Fifth Floor
+ *   Boston, MA  02110-1301  USA
+ */
+
+package li.klass.fhem.appwidget.ui.widget.medium
+
+import android.content.Context
+import android.widget.RemoteViews
+
+import li.klass.fhem.R
+import li.klass.fhem.appwidget.ui.widget.base.DeviceAppWidgetView
+import li.klass.fhem.appwidget.update.WidgetConfiguration
+import li.klass.fhem.dagger.ApplicationComponent
+import li.klass.fhem.domain.core.FhemDevice
+
+class StatusWidgetView : DeviceAppWidgetView() {
+    override fun getWidgetName(): Int = R.string.widget_status
+
+    override fun getContentView(): Int = R.layout.appwidget_state
+
+    override fun fillWidgetView(context: Context, view: RemoteViews, device: FhemDevice, widgetConfiguration: WidgetConfiguration) {
+        setTextViewOrHide(view, R.id.status, device.state)
+        openDeviceDetailPageWhenClicking(R.id.main, view, device, widgetConfiguration, context)
+    }
+
+    override fun supports(device: FhemDevice, context: Context): Boolean = true
+
+    override fun inject(applicationComponent: ApplicationComponent) {
+        applicationComponent.inject(this)
+    }
+}
