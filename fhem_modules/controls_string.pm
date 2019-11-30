e34095770455bd557ee16d8690cbc578fca40aea
Matthias Klass <matthias.klass@gmail.com>
2019-11-30 15:11:16 +0100

    Update month in controls_gcmsend.txt

--- fhem_modules/controls_string.pm
+++ fhem_modules/controls_string.pm
@@ -8,7 +8,7 @@ my $file = "FHEM/98_gcmsend.pm";
 my $stat = stat($file);
 
 my $size = $stat->size;
-my $date = POSIX::strftime("%Y-%d-%m", localtime( $stat->mtime ));
+my $date = POSIX::strftime("%Y-%m-%d", localtime( $stat->mtime ));
 my $time = POSIX::strftime("%H:%M:%S", localtime( $stat->mtime ));
 
 print "UPD ${date}_${time} $size FHEM/98_gcmsend.pm"
