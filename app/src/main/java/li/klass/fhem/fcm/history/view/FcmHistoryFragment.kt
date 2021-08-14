a76f958b054c7f1b2c1a0e72b5c37a19a079165c
Matthias Klass <matthias.klass@tngtech.com>
2021-08-14 14:27:04 +0200

    Migrate FcmHistoryFragment.kt to viewbinding

--- app/src/main/java/li/klass/fhem/fcm/history/view/FcmHistoryFragment.kt
+++ app/src/main/java/li/klass/fhem/fcm/history/view/FcmHistoryFragment.kt
@@ -4,8 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.fcm_history.view.*
-import li.klass.fhem.R
+import li.klass.fhem.databinding.FcmHistoryBinding
 import li.klass.fhem.fragments.core.BaseFragment
 import javax.inject.Inject
 
@@ -15,14 +14,16 @@ class FcmHistoryFragment @Inject constructor(
 ) : BaseFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fcm_history, container, false)
+        val viewBinding = FcmHistoryBinding.inflate(inflater)
         val myActivity = activity ?: return null
 
-        view?.viewpager?.adapter = FcmFragmentPagerAdapter(myActivity, childFragmentManager,
-                fcmHistoryMessagesFragment, fcmHistoryUpdatesFragment)
-        view?.tabs?.setupWithViewPager(view.viewpager)
+        viewBinding.viewpager.adapter = FcmFragmentPagerAdapter(
+            myActivity, childFragmentManager,
+            fcmHistoryMessagesFragment, fcmHistoryUpdatesFragment
+        )
+        viewBinding.tabs.setupWithViewPager(viewBinding.viewpager)
 
-        return view
+        return viewBinding.root
     }
 
     override suspend fun update(refresh: Boolean) {
