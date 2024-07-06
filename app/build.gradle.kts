90c9151e18b3c26f61264aec5dad7d3f07ab8a10
Matthias Klass <matthias.klass@gmail.com>
2024-07-06 11:20:53 +0200

    Update crashlytics

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -162,7 +162,9 @@ dependencies {
     implementation(group = "com.github.bumptech.glide", name = "okhttp3-integration", version = glideVersion) {
         exclude(group = "glide-parent")
     }
-    implementation(group = "com.google.firebase", name = "firebase-crashlytics", version = "19.0.0")
+
+    implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
+    implementation("com.google.firebase:firebase-crashlytics")
 
     implementation(group = "org.jetbrains.kotlin", name = "kotlin-stdlib-jdk7", version = kotlinVersion)
     implementation(group = "org.jetbrains.kotlinx", name = "kotlinx-serialization-json", version = "1.6.3")
